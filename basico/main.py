543252aaf600dbdc8061ff113714c15a01bbf155
JorgeRGit <jorgeromerocagigas@gmail.com>
2024-10-15 10:49:25 +0200

    cambios en el método put para que se piuedan modificar todas las variables permitidas

--- basico/main.py
+++ basico/main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI, HTTPException, status
 from boton import Boton
+from pydantic import BaseModel, Field
 
 # app
 app = FastAPI()
@@ -50,15 +51,32 @@ def deleteBoton(nombre: str, status_code=status.HTTP_200_OK):
     raise HTTPException(status_code=404, detail="Item not found")
 
 
-@app.put("/botones/{nombre}")
-def updateBoton(nombre: str, nombreNuevo, status_code=status.HTTP_200_OK):
+class BotonUpdate(BaseModel):
+    nombreNuevo: str = Field(None, description="Nuevo nombre del Boton")
+    color: str = Field(None, description="Nuevo color del Boton (formato hexadecimal)")
+    estado: bool = Field(None, description="Nuevo estado del Boton")
 
-    # Falta de comprobar que no se cambie el nombre a uno que no exista.
-    # Tengo que educarme sobre como funciona Python con variables privadas
-    for index, boton in enumerate(lista_variable_botones):
+
+@app.put("/botones/{nombre}")
+def updateBoton(nombre: str, boton_update: BotonUpdate, status_code=status.HTTP_200_OK):
+
+    for boton in lista_variable_botones:
         if boton.nombre == nombre:
 
-            boton.nombre = nombreNuevo
+            try:
+
+                if boton_update.nombreNuevo is not None and boton_update.nombreNuevo != "":
+                    boton.nombre = boton_update.nombreNuevo
+
+                if boton_update.color is not None and boton_update.color != "":
+                    boton.color = boton_update.color
+
+            except:
+                raise HTTPException(status_code=500, detail="Nombre o color no validos.")
+
+            if boton_update.estado is not None:
+                boton.estado = boton_update.estado
+
             return boton
 
-    raise HTTPException(status_code=404, detail="Item not found")
+    raise HTTPException(status_code=404, detail="Botón no encontrado.")
